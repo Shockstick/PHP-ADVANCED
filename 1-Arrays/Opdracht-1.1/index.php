b7bbb5e26e09e53303e602fd6c1c7a511dd3e74c
Shockstick <2152301@talnet.nl>
2022-02-15 14:08:18 +0100

    Opdracht 1.1

---
+++ 1-Arrays/Opdracht-1.1/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$Kolom = array("Voornaam"=>"Youssef", "Achternaam"=>"Amzour", "Klas"=>"A1", "Leeftijd"=>"17");
+
+echo "<ul>";
+echo "<li>";
+echo "$Kolom[Voornaam]";
+echo "</li>";
+echo "<li>";
+echo "$Kolom[Achternaam]";
+echo "</li>";
+echo "<li>";
+echo "$Kolom[Klas]";
+echo "</li>";
+echo "<li>";
+echo "$Kolom[Leeftijd]";
+echo "</li>";
+echo "</ul>";
+?>
+
+</body>
+</html>
