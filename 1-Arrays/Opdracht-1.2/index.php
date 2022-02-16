8e70bb3383081cbd99b2b8b4d22d194f3c277c43
Shockstick <2152301@talnet.nl>
2022-02-16 12:28:49 +0100

    Opdracht 1.2

---
+++ 1-Arrays/Opdracht-1.2/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$Koning ["Voornaam"] = "Willem";
+$Koning["Achternaam"] = "Alexander";
+$Koning["Leeftijd"]  =  "53";
+$Koning["Opleiding"] =  "Software Ontwikkeling";
+
+echo "$Koning[Voornaam] $Koning[Achternaam] van Oranje is $Koning[Leeftijd] jaar oud en is student aan het ROC van Amsterdam aan de opleiding $Koning[Opleiding]";
+?>
+
+</body>
+</html>
