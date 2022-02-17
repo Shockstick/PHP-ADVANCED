fce6060c668a7b69a1402d3ff009e028eae60ef5
Shockstick <2152301@talnet.nl>
2022-02-17 11:49:38 +0100

    Opdracht 2.4

---
+++ 2-Loops/Opdracht-2.4/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php  
+$klassen = array("8A", "8B", "8C", "8D", "8E", "9A", "9B", "9C", "9D", "9E");
+
+echo "<fieldset style='border: solid 1px; width:150px;'><legend> klassenformulier </legend>";
+
+echo "Ik zit in klas <select>";
+
+foreach ($klassen as $value){
+echo "<option value=$value> $value</option>";
+}
+
+echo "</select> </br>";
+echo "<button> Verzend </button>";
+echo "</fieldset>"
+?>  
+
+</body>
+</html>
