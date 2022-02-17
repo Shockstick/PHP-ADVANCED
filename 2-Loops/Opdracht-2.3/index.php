f9c96fd3c2ebeb78b97ac7284d60efb52da91802
Shockstick <2152301@talnet.nl>
2022-02-17 11:34:27 +0100

    Opdracht 2.3

---
+++ 2-Loops/Opdracht-2.3/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php  
+$dagen = array("maandag", "dinsdag", "woensdag", "donderdag", "vrijdag", "zaterdag", "zondag"); 
+
+foreach ($dagen as $value) {
+  echo "<ul>
+  <li>$value</li>
+  </ul> <br>";
+}
+?>  
+
+</body>
+</html>
