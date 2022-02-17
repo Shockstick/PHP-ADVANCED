2444be1d8cde1719cc2b09a9eb3b185ce2ee3e0f
Shockstick <2152301@talnet.nl>
2022-02-17 11:18:52 +0100

    Opdracht 2.2

---
+++ 2-Loops/Opdracht-2.2/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php  
+
+$recht = 18;
+
+for($leeftijd = 0; $leeftijd < 18; $leeftijd++){
+    echo "Ik ben $leeftijd jaar, dus ik mag nog niet stemmen <br>";
+}
+echo "Ik ben $recht jaar oud dus ik heb stemrecht!";
+
+?>  
+
+</body>
+</html>
