95a071cde86860c5464cc49e4488fc0a0c935cef
Shockstick <2152301@talnet.nl>
2022-02-18 10:55:54 +0100

    Opdracht 3.1

---
+++ 3-Forms/Opdracht-3.1/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html>  
+<body>
+
+<form action="welcome.php" method="post">
+Voornaam: <input type="text" name="name"><br>
+Achternaam: <input type="text" name="name"><br>
+Klas: <input type="text" name="name"><br>
+Leeftijd: <input type="text" name="name"><br>
+Adres: <input type="text" name="name"><br>
+Woonplaats: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
