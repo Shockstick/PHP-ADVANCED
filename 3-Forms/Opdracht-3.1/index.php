7e5255406c4c85d19ac4f4d761ea4be1706e23d7
Shockstick <2152301@talnet.nl>
2022-02-18 10:57:11 +0100

    Fix

--- 3-Forms/Opdracht-3.1/index.php
+++ 3-Forms/Opdracht-3.1/index.php
@@ -3,12 +3,12 @@
 <body>
 
 <form action="welcome.php" method="post">
-Voornaam: <input type="text" name="name"><br>
-Achternaam: <input type="text" name="name"><br>
-Klas: <input type="text" name="name"><br>
-Leeftijd: <input type="text" name="name"><br>
-Adres: <input type="text" name="name"><br>
-Woonplaats: <input type="text" name="email"><br>
+Voornaam: <input type="text" name="Voornaam"><br>
+Achternaam: <input type="text" name="Achternaam"><br>
+Klas: <input type="text" name="Klas"><br>
+Leeftijd: <input type="text" name="Leeftijd"><br>
+Adres: <input type="text" name="Adres"><br>
+Woonplaats: <input type="text" name="Woonplaats"><br>
 <input type="submit">
 </form>
 
