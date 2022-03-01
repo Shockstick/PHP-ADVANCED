2e99cdaf31f66b0c35daae114eb9ef2bb2c7f69a
Shockstick <2152301@talnet.nl>
2022-03-01 13:22:30 +0100

    Fix

---
+++ 3-Forms/Opdracht-3.1/mijngegevens.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php  
+
+echo "Voornaam is:".$_POST["fname"]; 
+echo "<br>";
+echo "Achternaam is:".$_POST["lname"];
+echo "<br>";
+echo "Klas is:".$_POST["cname"];
+echo "<br>";
+echo "Leeftijd is:".$_POST["aname"];
+echo "<br>";
+echo "Adres is:".$_POST["adname"];
+echo "<br>";
+echo "Woonplaats is:".$_POST["rname"];
+echo "<br>";
+?>  
+
+</body>
+</html>
