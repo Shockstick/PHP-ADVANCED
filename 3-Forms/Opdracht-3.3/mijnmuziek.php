7ff3c9dc3e2a7d7501e854f120fb4f84a807c551
Shockstick <2152301@talnet.nl>
2022-03-04 10:26:03 +0100

    Opdracht 3.3

---
+++ 3-Forms/Opdracht-3.3/mijnmuziek.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html>  
+<body>
+<title>Muziek</title>
+
+<form action="mijnmuziek_get.php" method="get">
+Naam: <input type="text" name="Name"><br>
+Titel: <input type="text" name="Titel"><br>
+Jaar van uitkomst: <input type="Date" name="Year"><br>
+Muzieksoort: <input type="text" name="Type"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
---
+++ 3-Forms/Opdracht-3.3/mijnmuziek_get.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html>  
+<body>
+<title>Muziek</title>
+
+His name is <?php echo $_GET["Name"]; ?><br>
+His title's name is: <?php echo $_GET["Titel"]; ?><br>
+He was born in <?php echo $_GET["Year"]; ?><br>
+Your email address is: <?php echo $_GET["Type"]; ?>
+
+
+</body>
+</html>
