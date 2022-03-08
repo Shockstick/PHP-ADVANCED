4d6f95ebc4112e2b81e09118e78c87f8e2fdba6a
Shockstick <2152301@talnet.nl>
2022-03-08 13:36:26 +0100

    opdracht 4.1

---
+++ 4-Functions/Opdracht-4.1/Functies-1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML>
+<html>  
+<body>
+
+<?php
+
+function schrijfkleur($kleur) {
+    echo ("<p style=color:$kleur;>Deze tekst heeft de kleur $kleur.</p>");
+  }
+  
+  schrijfkleur("#FF0000");
+  schrijfkleur("green");
+  schrijfkleur("#0000FF");
+  schrijfkleur("yellow");
+?>
+
+    
+</body>
+</html>
